fc2f99d79511170914ff375c0309410d7ed3f057
Bhoomik shetty <shettybhoomik@gmail.com>
2026-04-07 00:33:54 +0530

    Query

--- src/main/java/com/example/jobqueue/Controller.java
+++ src/main/java/com/example/jobqueue/Controller.java
@@ -1,10 +1,14 @@
 package com.example.jobqueue;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+import java.util.List;
 
 @RestController
 public class Controller {
@@ -23,8 +27,16 @@ public class Controller {
     }
 
     @GetMapping("/getJobStatus")
-    public static JobStatus getJobStatus(){
-        return JobStatus.completed;
+    public static JobStatus getJobStatus(Long id){
+        Optional<Job> job = jobRepository.findById(id);
+        return job.get().status;
+    }
+
+
+    @GetMapping("/testEndpoint")
+    public static ResponseEntity testEndpoint(){
+        List<Job> jobs = jobRepository.fetchPendingJobs();
+        return ResponseEntity.status(200).body(jobs);
     }
 }
 
--- src/main/java/com/example/jobqueue/JobRepository.java
+++ src/main/java/com/example/jobqueue/JobRepository.java
@@ -1,6 +1,17 @@
 package com.example.jobqueue;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
+
+    @Query(value = """
+    SELECT * FROM jobs
+    WHERE status = 'PENDING'
+    FOR UPDATE SKIP LOCKED
+    LIMIT 10
+    """, nativeQuery = true)
+    List<Job> fetchPendingJobs();
 }
